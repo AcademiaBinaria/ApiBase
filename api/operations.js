d567b942e41dd5c1d92945027c69df9054381b60
Alberto Basalo <albertobasalo@hotmail.com>
2017-11-21 20:25:54 +0100

    filter operations by owner

--- api/operations.js
+++ api/operations.js
@@ -5,9 +5,12 @@ module.exports = (app, url) => {
   app
     .route(url)
     .get((req, res) => {
-      const userItems = items.find(i => (i.owner = req.email));
-      if (userItems && userItems.length > 0) res.json(userItems);
-      else res.status(204).send();
+      const userItems = items.filter(i => i.owner == req.email);
+      if (userItems && userItems.length > 0) {
+        res.json(userItems);
+      } else {
+        res.status(204).send();
+      }
     })
     .post((req, res) => {
       const item = req.body;
@@ -22,16 +25,20 @@ module.exports = (app, url) => {
     });
   // // api/priv/operations/count
   app.route(`${url}/count`).get((req, res) => {
-    const userItems = items.find(i => (i.owner = req.email));
-    res.json({ count: userItems.length });
+    const userItems = items.filter(i => i.owner == req.email);
+    const count = userItems ? userItems.length : 0;
+    res.json({ count: count });
   });
   // // api/priv/operations/314
   app
     .route(`${url}/:id`)
     .get((req, res) => {
       const index = getIndexByOwnerId(req.email, req.params.id);
-      if (index >= 0) res.json(items[index]);
-      else res.status(404).send();
+      if (index >= 0) {
+        res.json(items[index]);
+      } else {
+        res.status(404).send();
+      }
     })
     .put((req, res) => {
       const index = getIndexByOwnerId(req.email, req.params.id);
