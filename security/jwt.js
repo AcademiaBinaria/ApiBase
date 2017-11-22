83d53585357e86d56b73c6a0460b97c3b15f3595
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-11-22 13:27:21 +0100

    expires in une hour

--- security/jwt.js
+++ security/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const secret = "http://academia-binaria.com";
-const seconds = 60;
+const seconds = 60 * 60;
 
 exports.createToken = user => {
   const token = jwt.sign({ email: user.email }, secret, { expiresIn: seconds });
